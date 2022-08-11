36dbc9ede2b81bd692b0034e1a70cc3381aaf0b1
fayizv <fayizziyafv10@gmail.com>
2022-08-11 12:50:05 +0530

    first commit

---
+++ category/models.py
@@ -0,0 +1,24 @@
+from django.urls import reverse
+from distutils.command.upload import upload
+from tabnanny import verbose
+from xml.parsers.expat import model
+from django.db import models
+
+# Create your models here.
+
+class category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    detailes = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])  
+
+
+    def __str__(self):
+        return self.category_name
